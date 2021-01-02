45d14a67987e998e8a2c9c8f26b00eca11454361
JeongChangYong <ckddyd1202@daum.net>
2021-01-02 21:13:25 +0900

    200102

---
+++ TestCocoa/TestCocoa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestCocoa
+//
+//  Created by 정창용 on 2021/01/02.
+//
+
+import UIKit
+import DSMSDK
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
