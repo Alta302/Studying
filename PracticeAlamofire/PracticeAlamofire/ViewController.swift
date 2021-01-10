75353a05b8ed1ec84659ddc678541d1431d8aa02
JeongChangYong <ckddyd1202@daum.net>
2021-01-10 20:19:19 +0900

    210110

---
+++ PracticeAlamofire/PracticeAlamofire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeAlamofire
+//
+//  Created by 정창용 on 2021/01/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
