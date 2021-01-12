b266939acefb3b5950f58ca84885a227f8a15515
JeongChangYong <ckddyd1202@daum.net>
2021-01-12 11:15:41 +0900

    210112

---
+++ PracticeAlamofire/PracticeAlamofire/CustomCell.swift
@@ -0,0 +1,25 @@
+//
+//  CustomCell.swift
+//  PracticeAlamofire
+//
+//  Created by 정창용 on 2021/01/12.
+//
+
+import UIKit
+
+import SnapKit
+
+class CustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- PracticeAlamofire/PracticeAlamofire/ViewController.swift
+++ PracticeAlamofire/PracticeAlamofire/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let urlString = "https://api.androidhive.info/contacts/"
+    let tableView = UITableView()
+    var dataSourse: [Contect] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
+        
     }
-
+    
+    private func setupUI() {
+        self.view.addSubview(tableView)
+        self.tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview()
+            
+        }
+        
+    }
 
 }
 
