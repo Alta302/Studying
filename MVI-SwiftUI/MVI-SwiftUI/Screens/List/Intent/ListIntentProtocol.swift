4a1ab48d101f35a7ec3f10847013240da04b9bd8
Alta302 <ckddyd1202@daum.net>
2022-12-01 17:48:24 +0900

    [TIL] 221201 MVI-SwiftUI ListIntentProtocol

---
+++ MVI-SwiftUI/MVI-SwiftUI/Screens/List/Intent/ListIntentProtocol.swift
@@ -0,0 +1,11 @@
+//
+//  ListIntentProtocol.swift
+//  MVI-SwiftUI
+//
+//  Created by 정창용 on 2022/11/08.
+//  Finish
+
+protocol ListIntentProtocol {
+    func viewOnAppear()
+    func onTapUrlContent(id: String)
+}
