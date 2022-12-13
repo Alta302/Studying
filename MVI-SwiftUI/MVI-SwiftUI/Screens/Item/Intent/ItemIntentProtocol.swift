e0c3e326c1ec09c4fda422934d0eee6ec8f7785f
Alta302 <ckddyd1202@daum.net>
2022-12-13 16:03:52 +0900

    [TIL] 221213 MVI-SwiftUI ItemIntentProtocol

---
+++ MVI-SwiftUI/MVI-SwiftUI/Screens/Item/Intent/ItemIntentProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  ItemIntentProtocol.swift
+//  MVI-SwiftUI
+//
+//  Created by 정창용 on 2022/11/10.
+//
+
+protocol ItemIntentProtocol {
+    func viewOnAppear()
+    func viewOnDisappear()
+    func didTapPlaying()
+}
