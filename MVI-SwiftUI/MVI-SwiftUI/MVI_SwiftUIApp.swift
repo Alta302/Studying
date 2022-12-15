57c410c6dc43e682e1baacffbcf9ce88bc55b17b
Alta302 <ckddyd1202@daum.net>
2022-12-15 16:30:18 +0900

    [TIL] 221215 MVI-SwiftUI MVI_SwiftUIApp

--- MVI-SwiftUI/MVI-SwiftUI/MVI_SwiftUIApp.swift
+++ MVI-SwiftUI/MVI-SwiftUI/MVI_SwiftUIApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MVI_SwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ListView()
+            ListView.build(data: .init())
         }
     }
 }
