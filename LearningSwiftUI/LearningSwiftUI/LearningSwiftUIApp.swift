5937b4a0d1e3b979d60485992a9e380664266930
정창용 <ckddyd1202@daum.net>
2022-12-23 14:45:51 +0900

    [TIL] 221223 LearningSwiftUI LearningSwiftUIApp

---
+++ LearningSwiftUI/LearningSwiftUI/LearningSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftUIApp.swift
+//  LearningSwiftUI
+//
+//  Created by 정창용 on 2022/11/10.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ListView()
+        }
+    }
+}
